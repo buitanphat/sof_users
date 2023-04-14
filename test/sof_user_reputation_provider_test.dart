6951a3f1ca04f26a9f352c0d6913acafb16136d1
Bui Tan Phat <phatbt@incognito.org>
2023-04-14 11:33:18 +0700

    Add unit test files

--- test/sof_user_reputation_provider_test.dart
+++ test/sof_user_reputation_provider_test.dart
@@ -1,1 +1,19 @@
+import 'package:sof_users/features/users/provider/sof_user_reputation_provider.dart';
+import 'package:test/test.dart';
 
+void main() {
+  group('SOFUserReputationProvider Testing....', () {
+    final provider = SOFUserReputationProvider();
+    provider.addListener(() {});
+
+    int page = 1;
+
+    test('reputation list should empty at the fist time!', () {
+      expect(provider.reputationList.length, 0);
+    });
+
+    test('add bookmark by userId', () {
+      // TO DO
+    });
+  });
+}
--- test/sof_users_provider_test.dart
+++ test/sof_users_provider_test.dart
@@ -1,1 +1,32 @@
+import 'package:sof_users/features/users/provider/sof_users_provider.dart';
+import 'package:test/test.dart';
 
+void main() {
+  group('SOFUsersProvider Testing....', () {
+    final usersProvider = SOFUsersProvider();
+    usersProvider.addListener(() {});
+
+    int page = 1;
+
+    test('userList and bookmark added should empty at the fist time!', () {
+      expect(usersProvider.userList.length, 0);
+      expect(usersProvider.bookmarkAddedMap.isEmpty, true);
+    });
+
+    test('fetch user data list with page index', () async {
+      // TO DO
+    });
+
+    test('add bookmark by userId', () {
+      // TO DO
+    });
+
+    test('remove bookmark by userId', () {
+      // TO DO
+    });
+
+    test('filter bookmark added', () {
+      // TO DO
+    });
+  });
+}
